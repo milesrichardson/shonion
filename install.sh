a06904b3f4a629a99b2d29f4936e92a9fae89dcb
Miles Richardson <milesrichardson@gmail.com>
2022-10-21 19:08:03 +0000

    Get listener side working

--- install.sh
+++ install.sh
@@ -8,17 +8,204 @@ SHONION_REPO="${SHONION_REPO-"https://github.com/milesrichardson/shonion"}"
 SHONION_BIN_URL="${SHONION_BIN_URL}"
 SHONION_BUILD_DIR="${SHONION_BUILD_DIR-"$HOME/.shonion"}"
 SHONION_UPDATE="${SHONION_UPDATE-"0"}"
-SHONION_CLIENT="${SHONION_CLIENT-"1"}"
-SHONION_LISTENER="${SHONION_LISTENER-"1"}"
 SHONION_FORK_TO_CLIENT="${SHONION_FORK_TO_CLIENT-"0"}"
 SHONION_FORK_TO_LISTENER="${SHONION_FORK_TO_LISTENER-"0"}"
+SHONION_LISTENER_ROOT="${SHONION_LISTENER_ROOT-""}"
+
+SHONION_TOR_ROOT="/tmp/tor-rust"
 
 main() {
-  _configure && _require_deps && _download_shonion && _validate_env
+  _parse_cli_opts "$@"
+
+  _configure && _require_deps && _download_shonion && _validate_env && fork_to_work
+}
+
+fork_to_work() {
+  if _should_fork_to_listener ; then
+    _fork_to_listener
+  elif _should_fork_to_client ; then
+    _fork_to_client
+  else
+    _log "Not forking to any new process"
+    _log "      to fork to client: SHONION_FORK_TO_CLIENT=1 $0"
+    _log "    to fork to listener: SHONION_FORK_TO_LISTENER=1 $0"
+  fi
+}
+
+# _wait_for_shutdown() {
+#   until [ -f "$hostname_file" ]
+#   do
+#       sleep 5
+#   done
+# }
+
+PID_SHONION=
+_cleanup() {
+  echo "Kill Shonion ($PID_SHONION)"
+  kill -9 "$PID_SHONION" || true
+
+  echo "KILL SSHD FROM" "$SHONION_LISTENER_ROOT"/sshd.pid
+  xargs kill -9 < "$SHONION_LISTENER_ROOT"/sshd.pid || true
+}
+
+_fork_to_listener() {
+  _log "[TODO] forking to listener ...."
+
+  "$SHONION_BIN" &
+  PID_SHONION=$!
+  trap 'echo CLEANUP shonion pid $PID_SHONION ; kill $PID_SHONION' INT
+
+  _wait_for_onion_hostname
+
+  _setup_listener_dir
+
+  /usr/sbin/sshd \
+-o Port=5678 \
+-o StrictModes=no \
+-o HostKey="$SHONION_LISTENER_ROOT"/ssh_host_rsa_key \
+-o PidFile="$SHONION_LISTENER_ROOT"/sshd.pid \
+-o KbdInteractiveAuthentication=no \
+-o ChallengeResponseAuthentication=no \
+-o PasswordAuthentication=no \
+-o UsePAM=yes \
+-o AuthorizedKeysFile="$SHONION_LISTENER_ROOT"/authorized_keys
+
+  _log "sshd.pid: $SHONION_LISTENER_ROOT/sshd.pid"
+
+  _print_client_instructions
+
+  wait "$PID_SHONION"
+
+  _log "Done ."
+}
+
+
+_wait_for_onion_hostname() {
+  hostname_file="$SHONION_TOR_ROOT/hs-dir/hostname"
+  _log "Wait for .onion hostname: $hostname_file ..."
+  until [ -f "$hostname_file" ]
+  do
+      sleep 5
+  done
+  _log "found hostname: $(_get_onion_hostname)"
+}
+
+# _wait_for_tor_network() {
+#   _log "Wait for Tor network to be up ..."
+#   until [ -n "$()" ]
+#   do
+#       sleep 5
+#   done
+# }
+
+_get_onion_hostname() {
+  cat "$SHONION_TOR_ROOT/hs-dir/hostname"
+}
+
+_fork_to_client() {
+  _log "[TODO] forking to client ...."
+}
+
+_setup_listener_dir() {
+  test -n "$SHONION_LISTENER_ROOT" || SHONION_LISTENER_ROOT="$(mktemp -d)"
+
+  test -n "$SHONION_LISTENER_ROOT" || _fatal "SHONION_LISTENER_ROOT not defined"
+  test -d "$SHONION_LISTENER_ROOT" || _fatal "SHONION_LISTENER_ROOT $SHONION_LISTENER_ROOT is not dir"
+  mkdir -p "$SHONION_LISTENER_ROOT"
+
+  _log "[LISTENER ROOT] $SHONION_LISTENER_ROOT"
+
+  trap '_cleanup' INT
+
+  _compile_client_script
+
+  _log "[CLIENT SCRIPT]" "$SHONION_LISTENER_ROOT/connect.sh"
+  _log "---"
+  cat "$SHONION_LISTENER_ROOT/connect.sh"
+  _log "---"
+}
+
+_print_client_instructions() {
+  echo
+  _log "Listening..."
+  _log "To connect from another machine, paste this into a terminal:"
+
+  tee <<EOC
+bash <(echo "$(base64 -w0 "$SHONION_LISTENER_ROOT"/connect.sh)" | base64 -D)
+EOC
+}
+
+_compile_client_script() {
+
+  echo "CWD IS: $(pwd)"
+  cd "$SHONION_LISTENER_ROOT" || _fatal "failed cd $SHONION_LISTENER_ROOT"
+  echo "CWD IS: $(pwd)"
+
+  touch sshd.pid \
+  && ssh-keygen -f id_shonion_client_rsa -N '' \
+  && cat id_shonion_client_rsa.pub > authorized_keys \
+  && ssh-keygen -f ssh_host_rsa_key -N ''
+
+cat <<EOC > "$SHONION_LISTENER_ROOT/connect.sh"
+#!/usr/bin/env bash
+
+cd \$(mktemp -d)
+
+tee id_shonion_client_rsa <<EOX
+$(cat id_shonion_client_rsa)
+EOX
+
+tee id_shonion_client_rsa.pub <<EOX
+$(cat id_shonion_client_rsa.pub)
+EOX
+
+chmod 0400 id_shonion_client_rsa id_shonion_client_rsa.pub
+
+exec ssh -v \\
+    -F /dev/null \\
+    -o IdentityFile=\$PWD/id_shonion_client_rsa \\
+    -o IdentitiesOnly=yes \\
+    -o ConnectTimeout=120 \\
+    -o StrictHostKeychecking=no \\
+    -o UserKnownHostsFile=/dev/null \\
+    -o "proxyCommand=nc -x 127.0.0.1:19050 -X 5 %h %p" \\
+$(whoami)@$(cat /tmp/tor-rust/hs-dir/hostname) -p 34567
+EOC
+
+  cd - || _fatal "failed cd back"
+  echo "CWD IS: $(pwd)"
+}
+
+_parse_cli_opts() {
+  while test "$#" -gt 0 ; do
+    nextarg="$1"
+    case "$nextarg" in
+      --connect)  shift ; SHONION_FORK_TO_CLIENT=1 ;;
+      --listen) shift ; SHONION_FORK_TO_LISTENER=1 ;;
+    esac
+  done
+}
+
+_should_fork_to_client() {
+  test "$SHONION_FORK_TO_CLIENT" = "1" || return 1
+  return 0
+}
+
+_should_fork_to_listener() {
+  test "$SHONION_FORK_TO_LISTENER" = "1" || return 1
+  return 0
 }
 
 _validate_env() {
   _dump_env
+
+  test -n "$SHONION_BIN" || _fatal "SHONION_BIN is not set"
+  test -x "$SHONION_BIN" || _fatal "SHONION_BIN is not executable at $SHONION_BIN"
+
+  if test "$SHONION_FORK_TO_CLIENT" = "1" && test "$SHONION_FORK_TO_LISTENER" = "1" ; then
+    _fatal "cannot fork to both client and server"
+  fi
+
 }
 
 _dump_env() {
@@ -27,26 +214,21 @@ _dump_env() {
   echo "SHONION_BIN=$SHONION_BIN"
   echo "SHONION_BUILD_DIR=$SHONION_BUILD_DIR"
   echo "SHONION_UPDATE=$SHONION_UPDATE"
-  echo "SHONION_LISTENER=$SHONION_LISTENER"
-  echo "SHONION_CLIENT=$SHONION_CLIENT"
+  echo "SHONION_FORK_TO_LISTENER=$SHONION_FORK_TO_LISTENER"
+  echo "SHONION_FORK_TO_CLIENT=$SHONION_FORK_TO_CLIENT"
+  echo "SHONION_LISTEN_ROOT=$SHONION_LISTEN_ROOT"
 }
 
 _require_deps() {
-  _require_dep "base64"
+  _require_dep "base64" "base64"
 
-  if ! _check_dep wget wget ; then
-    if ! _check_dep curl curl ; then
-      _fatal "either wget or curl is required, but neither is installed"
-    fi
-  fi
+  # if _is_client ; then
+  #   _require_dep "ssh" "openssh-client"
+  # fi
 
-  if _is_client ; then
-    _require_dep "ssh" "openssh-client"
-  fi
-
-  if _is_listener ; then
-    _require_dep "sshd" "openssh-server"
-  fi
+  # if _is_listener ; then
+  #   _require_dep "sshd" "openssh-server"
+  # fi
 }
 
 _require_dep() {
@@ -145,17 +327,27 @@ _fetch() {
   dest="$1"
   shift
 
-  _fetch_wget() { echo "FETCH via wget" && wget -O "$dest" "$url" && return 1 ; }
-  _fetch_curl() { echo "FETCH via curl" && curl -L "$url" -o "$dest" ; }
+  _fetch_wget() { _log "[FETCH via wget]" && wget -O "$dest" "$url" ; }
+  _fetch_curl() { _log "[FETCH via curl]" && curl -L "$url" -o "$dest" ; }
 
-  if _is_command wget ; then
+  if _check_dep wget wget ; then
     if ! _fetch_wget ; then
+      _log "wget download failed, trying curl..."
       if _check_dep curl curl ; then
-        _fetch_curl || _fatal "download failed"
+        _fetch_curl || _fatal "download failed via wget and via curl"
+      else
+        _fatal "download failed via wget, and curl is not available"
       fi
     fi
   elif _check_dep curl curl ; then
-    _fetch_curl || _fatal "download failed"
+    if ! _fetch_curl ; then
+      _log "curl download failed, trying wget..."
+      if _check_dep wget wget ; then
+         _fetch_wget || _fatal "download failed via curl and via wget"
+      else
+        _fatal "download failed via curl, and wget is not available"
+      fi
+    fi
   fi
 
   _log "download success"
@@ -205,14 +397,5 @@ _fatal() {
   exit 1
 }
 
-_is_client() {
-  test "$SHONION_CLIENT" = "1" && return 0
-  return 1
-}
-
-_is_listener() {
-  test "$SHONION_LISTENER" = "1" && return 0
-  return 1
-}
 
 main "$@"
---
+++ tests/test-installer.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+# Script to automate running install.sh on multiple platforms.
+# Does not make any assertions.
+#
+
+cd "$(dirname "$(git rev-parse --git-dir)")" || { echo "bad cd" ; exit 2 ; }
+
+PASSED=()
+FAILED=()
+
+RESULTS="$(mktemp -d)"
+
+main() {
+
+  if test "$#" -gt 0 ; then
+    while test "$#" -gt 0 ; do
+      run_installer "$1"
+      shift
+    done
+  else
+    run_tests
+  fi
+
+  report_results
+}
+
+run_tests() {
+  run_installer alpine:latest
+  run_installer ubuntu:latest
+}
+
+report_results() {
+
+  num_passed=${#PASSED[@]}
+  num_failed=${#FAILED[@]}
+
+  _log ""
+  _log "---"
+
+  if test "$num_passed" -gt 0 ; then
+    _log "PASSED  ($num_passed) >" "${PASSED[@]}"
+  fi
+
+  if test "$num_failed" -gt 0 ; then
+    _log "FAILED  ($num_failed) >" "${FAILED[@]}"
+    if test "$num_passed" -gt 0 ; then
+      _log "RESULTS     : some tests failed"
+    else
+      _log "RESULTS     : all tests failed"
+    fi
+  else
+    _log "RESULTS     : all tests passed"
+  fi
+
+  _log ""
+  _log "Logs saved to disk:"
+  find "$RESULTS" -type f
+}
+
+run_installer() {
+  local docker_ref="$1"
+  shift
+
+  _log "[RUN INSTALLER]" "$docker_ref" sh install.sh
+
+  set -o pipefail
+  if docker run --rm -it -v "$(pwd)":/app -w /app "$docker_ref" sh install.sh | tee "$(_log_to "$docker_ref")" ; then
+    PASSED+=("$docker_ref")
+  else
+    FAILED+=("$docker_ref")
+  fi
+  set +o pipefail
+}
+
+_log_to() {
+  local docker_ref="$1"
+  echo -n "$RESULTS/${docker_ref//:/__}".stdout.log
+}
+
+_log() { echo "$@" >&2 ; }
+
+main "$@"
+
+
+
