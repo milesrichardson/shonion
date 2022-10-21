584a2bf92f0ef4d1c6927b6aa65fb8f869a0f69d
Miles Richardson <miles@splitgraph.com>
2022-10-21 05:46:07 +0100

    [bin] build bin/static from f81b5e11

--- scripts/commit-release.sh
+++
@@ -1,9 +0,0 @@
-cd "$(dirname "$(git rev-parse --git-dir)")"
-
-executable_name="shonion-$(./scripts/get-host-triple.sh)"
-executable_path="bin/$executable_name"
-
-cp target/release/shonion "$executable_path"
-git add "$executable_path"
-current_commitish=$(git rev-parse HEAD | cut -c-8)
-git commit -m '[bin] update `'"$executable_name"'` (built from '"$current_commitish"')'
